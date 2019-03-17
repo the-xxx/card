21584b77f943c7aa05c74dc2fe4bbddee04e1114
gaosiyigesuanyige <593218846@qq.com>
2019-03-17 19:51:57 +0800

    发牌，比大小（规则完整）

--- assets/Script/card.js
+++ assets/Script/card.js
@@ -24,30 +24,160 @@ cc.Class({
             default: null,
             type: cc.Sprite
         },
+        node1: {
+            default: null,
+            type: cc.Node
+        },
+        node2: {
+            default: null,
+            type: cc.Node
+        },
+        node3: {
+            default: null,
+            type: cc.Node
+        },
     },
 
-    initCard: function(idx1, idx2, idx3){
-        this.loadRes(this.card1, idx1);
-        this.loadRes(this.card2, idx2);
-        this.loadRes(this.card3, idx3);
+    /*
+    * 初始化牌组
+    */
+    initCardGroup: function(cardIdx1, cardIdx2, cardIdx3){
+        this.cardInfo = [cardIdx1,cardIdx2,cardIdx3];
+        this.cardInfo.sort(
+            function(a, b){
+                if (a%100 == b%100){
+                    return b/100 - a/100;
+                }
+                return a%100 - b%100;
+            }
+        );
+        this.cardCompareInfo = {};
+        this.loadCard(this.node1, this.cardInfo[0]);
+        this.loadCard(this.node2, this.cardInfo[1]);
+        this.loadCard(this.node3, this.cardInfo[2]);
     },
 
-    loadRes: function(cardSpr, residx){
-        var type = Math.round(residx / 100);
-        var idx = Math.round(residx % 100);
-        // loadres for "resources", needn't extensions
-        var sPath = "card/card_big_symbol_number_"+this.typeKey[type]+"_"+this.cardKey[idx];
-        cc.log(sPath, type, idx, residx);
-        cc.loader.loadRes(sPath, cc.SpriteFrame, function (err, spriteFrame){
-            cardSpr.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+    /*
+    * 加载单张牌
+    * 异步加载
+    */
+    loadCard: function(cardNode, cardIdx){
+        let cardColor = Math.round(cardIdx / 100);
+        let cardVal = Math.round(cardIdx % 100);
+        cardNode.setScale(0.7);
+
+        let sPath = "card/card_bg_"+this.typeKey[cardColor];
+        cc.loader.loadRes(sPath, cc.SpriteFrame, function(err, spriteFrame){
+            let node = cardNode.getChildByName("bg");
+            if (node){
+                node.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+                return
+            }
+            node = new cc.Node("bg");
+            let sprite = node.addComponent(cc.Sprite);
+            sprite.spriteFrame = spriteFrame;
+            cardNode.addChild(node, -1);
+            node.setPosition(0, 0);
         });
+
+        sPath = "card/card_symbol_"+this.typeKey[cardColor];
+        cc.loader.loadRes(sPath, cc.SpriteFrame, function(err, spriteFrame){
+            let node = cardNode.getChildByName("type");
+            if (node){
+                node.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+                return
+            }
+            node = new cc.Node("type");
+            let sprite = node.addComponent(cc.Sprite);
+            sprite.spriteFrame = spriteFrame;
+            cardNode.addChild(node);
+            node.setPosition(1, -21);
+            node.setScale(2);
+        });
+
+        sPath = "card/num_"+this.colorKey[cardColor]+"_"+this.valueKey[cardVal];
+        cc.loader.loadRes(sPath, cc.SpriteFrame, function(err, spriteFrame){
+            let node = cardNode.getChildByName("value");
+            if (node){
+                node.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+                return
+            }
+            node = new cc.Node("value");
+            let sprite = node.addComponent(cc.Sprite);
+            sprite.spriteFrame = spriteFrame;
+            cardNode.addChild(node);
+            node.setPosition(0, 50);
+        });
+    },
+
+    getCardInfo: function(){
+        let numMinColor, numMin;
+        let numMidColor, numMid;
+        let numMaxColor, numMax;
+        [numMinColor, numMin] = [Math.round(this.cardInfo[0]/100), this.cardInfo[0]%100];
+        [numMidColor, numMid] = [Math.round(this.cardInfo[1]/100), this.cardInfo[1]%100];
+        [numMaxColor, numMax] = [Math.round(this.cardInfo[2]/100), this.cardInfo[2]%100];
+        return [numMinColor, numMin, numMidColor, numMid, numMaxColor, numMax];
+    },
+
+    /*
+     * 豹子：3相同
+     */
+    calTripleKill: function(numMinColor, numMin, numMidColor, numMid, numMaxColor, numMax){
+        return numMin == numMax;
+    },
+
+    /*
+     * 对子：2相同
+     */
+    calDoubleKill: function(numMinColor, numMin, numMidColor, numMid, numMaxColor, numMax){
+        return numMin == numMid || numMid == numMax;
+    },
+
+    /*
+     * 同花：牌色一致
+     */
+    calGoldenFlower: function(numMinColor, numMin, numMidColor, numMid, numMaxColor, numMax){
+        return numMinColor == numMidColor && numMinColor == numMaxColor;
+    },
+
+    /*
+     * 顺子：有序
+     * A23最小，QKA最大，用mid值来定位顺子大小标志，注：KA2不算顺子
+     */
+    calStraight: function(numMinColor, numMin, numMidColor, numMid, numMaxColor, numMax){
+        if ([numMin, numMid, numMax] == [0, 1, 12]){    // A23
+            return true
+        }
+        return numMin+1 == numMid && numMid+1 == numMax;
+    },
+
+    /*
+    * 获取比较信息
+    * 豹子 > 同花顺 > 同花 > 顺子 > 对子 > 单张
+    */
+    getCompareInfo: function(){
+        if (Object.keys(this.cardCompareInfo).length == 0) {
+            let [numMinColor, numMin, numMidColor, numMid, numMaxColor, numMax] = this.getCardInfo();
+            this.cardCompareInfo = {    // 比较时候用到的信息
+                "TripleKill": this.calTripleKill(numMinColor, numMin, numMidColor, numMid, numMaxColor, numMax),
+                "DoubleKill": this.calDoubleKill(numMinColor, numMin, numMidColor, numMid, numMaxColor, numMax),
+                "GoldenFlower": this.calGoldenFlower(numMinColor, numMin, numMidColor, numMid, numMaxColor, numMax),
+                "Straight": this.calStraight(numMinColor, numMin, numMidColor, numMid, numMaxColor, numMax),
+                "value" : [numMinColor, numMin, numMidColor, numMid, numMaxColor, numMax]
+            }
+        }
+        return this.cardCompareInfo;
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
-        this.typeKey = ['red','black'];
-        this.cardKey = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+        this.colorKey = ['red','black','red','black'];
+        this.typeKey = ['hongtao','heitao','fangkuai','meihua'];
+        this.valueKey = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+        this.cardInfo = [];
+        this.cardCompareInfo = {};
     },
 
     start () {
--- assets/Script/game.js
+++ assets/Script/game.js
@@ -20,50 +20,166 @@ cc.Class({
             default: null,
             type: cc.Button
         },
+        comparebutton: {
+            default: null,
+            type: cc.Button
+        },
     },
 
     createCard: function(){
         // get residx for random
-        var residx1 = Math.round(Math.random()) * 100 + Math.round(Math.random() * 12);
-        var residx2 = Math.round(Math.random()) * 100 + Math.round(Math.random() * 12);
-        var residx3 = Math.round(Math.random()) * 100 + Math.round(Math.random() * 12);
-        var newCard = cc.instantiate(this.cardPrefab);
-        this.node.addChild(newCard);
-        newCard.instance.game = this;
-        newCard.instance.initCard(residx1, residx2, residx3);
+        let cardIdx1 = Math.round(Math.random()*3) * 100 + Math.round(Math.random() * 12);
+        let cardIdx2 = Math.round(Math.random()*3) * 100 + Math.round(Math.random() * 12);
+        let cardIdx3 = Math.round(Math.random()*3) * 100 + Math.round(Math.random() * 12);
+        let newCard;
+        if (this.cardCache.length == 0) {
+            newCard = cc.instantiate(this.cardPrefab);
+            this.node.addChild(newCard);
+            newCard.instance.game = this;
+        }
+        else{
+            newCard = this.cardCache.pop();
+            newCard.active = true;
+        }
+        newCard.instance.initCardGroup(cardIdx1, cardIdx2, cardIdx3);
         return newCard;
     },
 
     gamebegin: function(){
         if (this.cardList.length > 0){
-            for (var idx in this.cardList){
-                this.cardList[idx].destroy();   // release node
+            for (let idx in this.cardList){
+                this.cardList[idx].active = false;
+                this.cardCache.push(this.cardList[idx]);
+                // this.cardList[idx].destroy();   // release node
             }
         }
         this.cardList = [];
-        for ( var pos in this.posList){
-            var newCard = this.createCard();
+        for ( let pos in this.posList){
+            let newCard = this.createCard();
             newCard.setPosition(0, this.posList[pos]);
             this.cardList.push(newCard);
         }
     },
 
+    /*
+    * 0-都没有 1-仅1有 2-仅2有 3-都有
+    */
+    checkKey: function(cardInfo1, cardInfo2, sKey){
+        var val = 0;
+        cc.log(cardInfo1[sKey],cardInfo2[sKey]);
+        if (cardInfo1[sKey]){
+            val |= 1;
+        }
+        if (cardInfo2[sKey]){
+            val |= 2;
+        }
+        return val
+    },
+
+    /*
+    * 单牌大小
+    * 1-1号大 2-2号大
+    */
+    checkOneCard: function(cardInfo1, cardInfo2, iIdx){
+        let [numColor1, num1] = [cardInfo1["value"][iIdx], cardInfo1["value"][iIdx+1]];
+        let [numColor2, num2] = [cardInfo2["value"][iIdx], cardInfo2["value"][iIdx+1]];
+        if (num1 != num2){
+            return (num1>num2)?1:2;   // 牌面值大小，0~12表示牌值2~A
+        }
+        else{
+            return (numColor1<numColor2)?1:2; // 花色大小，0~3表示花色红黑方梅，依次从大到小
+        }
+    },
+
+    /*
+    * 1-1号赢 2-2号赢
+    */
+    doCompareCard: function(oCard1, oCard2){
+        let cardInfo1 = oCard1.instance.getCompareInfo();
+        let cardInfo2 = oCard2.instance.getCompareInfo();
+        cc.log(">>>>>cardInfo1");
+        for(let i in cardInfo1){
+            cc.log(i,cardInfo1[i]);
+        }
+        cc.log(">>>>>cardInfo2");
+        for(let i in cardInfo2){
+            cc.log(i,cardInfo2[i]);
+        }
+
+        // "TripleKill"
+        // "DoubleKill"
+        // "GoldenFlower"
+        // "Straight"
+        // "value" : [numMinColor, numMin, numMidColor, numMid, numMaxColor, numMax]
+
+        let valOneCard = this.checkOneCard(cardInfo1, cardInfo2, 4);   // 仅比单个牌的大小
+        cc.log('-----------------valOneCard',valOneCard);
+        let valMidCard = this.checkOneCard(cardInfo1, cardInfo2, 2);   // 仅比单个牌的大小
+        cc.log('-----------------valMidCard',valMidCard);
+
+        let valThipleKill = this.checkKey(cardInfo1, cardInfo2, "TripleKill");
+        cc.log('-----------------valThipleKill',valThipleKill);
+        if (valThipleKill){ //豹子
+            return (valThipleKill==3)?valOneCard:valThipleKill;
+        }
+        let valGoldenFlower = this.checkKey(cardInfo1, cardInfo2, "GoldenFlower");
+        cc.log('-----------------valGoldenFlower',valGoldenFlower);
+        let valStraight = this.checkKey(cardInfo1, cardInfo2, "Straight");
+        cc.log('-----------------valStraight',valStraight);
+        if (valGoldenFlower & valStraight){    // 同花顺
+            return ((valGoldenFlower & valStraight)==3)?valOneCard:(valGoldenFlower & valStraight);
+        }
+        if (valGoldenFlower){   // 同花
+            return (valGoldenFlower==3)?valOneCard:valGoldenFlower;
+        }
+        if (valStraight){   // 顺子
+            return (valStraight==3)?valOneCard:valStraight;
+        }
+        let valDoubleKill = this.checkKey(cardInfo1, cardInfo2, "DoubleKill");
+        cc.log('-----------------valDoubleKill',valDoubleKill);
+        if (valDoubleKill){ // 对子
+            return (valDoubleKill==3)?valOneCard:valDoubleKill;
+        }
+        return valOneCard;  // 单张
+    },
+
     callback: function(event, customEventData){
-        this.gamebegin();
+        cc.log(event, customEventData, '+++++++++++++++++++++++++++');
+        if (customEventData == "dealbutton") {
+            this.gamebegin();
+        }
+        else{
+            if (this.cardList.length){
+                let val = this.doCompareCard(this.cardList[0], this.cardList[1]);
+                cc.log("结果",val);
+                let delKey = (val==1)?1:0;
+                this.cardCache.push(this.cardList[delKey]);
+                this.cardList[delKey].active = false;
+                this.cardList.splice(delKey, 1);
+            }
+        }
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
         this.cardList = [];
-        this.posList = [0,300];
+        this.cardCache = [];
+        this.posList = [-169,169];
 
-        var clickEventHandler = new cc.Component.EventHandler();
+        let clickEventHandler = new cc.Component.EventHandler();
         clickEventHandler.target = this.node;
         clickEventHandler.component = "game";
         clickEventHandler.handler = "callback";
-        clickEventHandler.customEventData = "";
+        clickEventHandler.customEventData = "dealbutton";
         this.dealbutton.clickEvents.push(clickEventHandler);
+
+        let clickEventHandler2 = new cc.Component.EventHandler();
+        clickEventHandler2.target = this.node;
+        clickEventHandler2.component = "game";
+        clickEventHandler2.handler = "callback";
+        clickEventHandler2.customEventData = "comparebutton";
+        this.comparebutton.clickEvents.push(clickEventHandler2);
     },
 
     start () {
